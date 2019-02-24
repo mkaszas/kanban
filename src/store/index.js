86b7460b9de12cf6fbfd245abd15edf98985ce40
Mate Kaszas <mate.kaszas@festicket.com>
2019-02-24 22:02:27 +0000

    configure redux localstorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,15 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { save, load } from 'redux-localstorage-simple';
 import rootReducer from './reducers';
 
-export default createStore(
+const namespace = 'mk_sp_kanban';
+
+const createStoreWithMiddleware = applyMiddleware(
+  save({ namespace, debounce: 1000 })
+)(createStore);
+
+export default createStoreWithMiddleware(
   rootReducer,
+  load({ namespace }),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
